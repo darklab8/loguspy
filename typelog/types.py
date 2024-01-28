0ccb24eda18f5a53225c8d5c2612abf710eca581
dd84ai <dd84ai@gmail.com>
2024-01-28 21:55:58 +0100

    refactor: fix minor typing dict problem

--- typelog/types.py
+++ typelog/types.py
@@ -2,7 +2,7 @@ from typing import Any, Callable, Dict, NewType
 
 Serialazable = Any
 
-LogAttrs = NewType("LogAttrs", Dict[str, Serialazable])
+LogAttrs = Dict[str, Serialazable]
 LogType = Callable[[LogAttrs], None]
 
 
