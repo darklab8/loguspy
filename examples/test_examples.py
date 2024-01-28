25d196f56d26e88230426f4bfc94b231b5e173d4
dd84ai <dd84ai@gmail.com>
2024-01-28 02:28:08 +0100

    feat: configurator of loggers

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -1,15 +1,37 @@
 import unittest
 from logus import get_logger
+import logus
 from . import app_logus
 from . import app_types
+from logus.utils import Loggers, LibName, LogLevel
+import logging
 
 logger = get_logger(__name__)
 
 
 class TestExamples(unittest.TestCase):
-    def test_basic(self):
-        logger.warn("Writing something", app_logus.TaskID(123))
 
-    def test_another_one(self):
+    def setUp(self) -> None:
+        Loggers(
+            root_log_level=logging.DEBUG,
+            log_levels={
+                LibName("examples"): LogLevel(logging.DEBUG),
+            }, 
+            add_time=True,
+        ).configure()
+
+    def test_basic(self) -> None:
+        logger.warn("Writing something", app_logus.TaskID(app_types.TaskID(123)))
+
+    def test_another_one(self) -> None:
         task = app_types.Task(smth="abc", b=4)
         logger.warn("Writing something", app_logus.Task(task))
+
+    def test_with_fields(self) -> None:
+
+        logger2 = logger.with_fields(app_logus.Task(app_types.Task(smth="aaa", b=1)))
+        logger3 = logger.with_fields(logus.String("smth", "asd"), logus.Int("number", 2))
+
+        logger.info("logger printed")
+        logger2.info("logger2 printed")
+        logger3.info("logger3 printed")
--- logus/logcore.py
+++ logus/logcore.py
@@ -1,6 +1,7 @@
 import json
 import logging
-from typing import Dict
+from typing import Dict, List, Optional
+from copy import deepcopy
 
 from . import settings
 from .logtypes import LogOption, LogParams
@@ -15,15 +16,17 @@ class StructuredMessage:
     def __init__(
         self,
         message: str,
+        turn_json: Optional[bool],
         *args: LogOption,
     ) -> None:
         self._message = message
+        self._turn_json = turn_json
         self._kwargs: LogParams = LogParams({})
         for add_option in args:
             add_option(self._kwargs)
 
     def __str__(self) -> str:
-        if settings.LOG_JSON:
+        if settings.LOG_JSON or self._turn_json:
             return json.dumps(self.to_dict())
 
         if len(self._kwargs.keys()) == 0:
@@ -42,28 +45,35 @@ sm = StructuredMessage
 
 
 class Logus:
-    def __init__(self, name: str):
+    def __init__(self, name: str, turn_json: Optional[bool]):
         """
         pass __file__ into file
         """
         self.logger = logging.getLogger(name)
         self.logger.setLevel(default_log_level)
+        self._with_fields: List[LogOption] = []
+        self._turn_json = turn_json
 
     def debug(self, message: str, *args: LogOption) -> None:
-        self.logger.debug(StructuredMessage(message, *args))
+        self.logger.debug(StructuredMessage(message,self._turn_json, *args))
 
     def info(self, message: str, *args: LogOption) -> None:
-        self.logger.info(StructuredMessage(message, *args))
+        self.logger.info(StructuredMessage(message, self._turn_json, *args))
 
     def warn(self, message: str, *args: LogOption) -> None:
-        self.logger.warning(StructuredMessage(message, *args))
+        self.logger.warning(StructuredMessage(message,self._turn_json,  *args))
 
     def error(self, message: str, *args: LogOption) -> None:
-        self.logger.error(StructuredMessage(message, *args))
+        self.logger.error(StructuredMessage(message,self._turn_json,  *args))
 
     def fatal(self, message: str, *args: LogOption) -> None:
-        self.logger.fatal(StructuredMessage(message, *args))
+        self.logger.fatal(StructuredMessage(message,self._turn_json,  *args))
+
+    def with_fields(self, *args: LogOption) -> "Logus":
+        logger = deepcopy(self)
+        logger._with_fields.extend(args)
+        return logger
 
 
-def get_logger(file: str) -> Logus:
-    return Logus(file)
+def get_logger(file: str, turn_json: Optional[bool] = None) -> Logus:
+    return Logus(file, turn_json)
